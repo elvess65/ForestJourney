745a49e01cac0c86e9d73d5636cc07982de8dbe5
Pavlo <elvess65@gmail.com>
2018-05-04 13:58:18 +0300

    1

--- Assets/Scripts/Main/CameraController.cs
+++ Assets/Scripts/Main/CameraController.cs
@@ -9,9 +9,12 @@ public class CameraController : MonoBehaviour
     public Vector3 CameraOffset;
 
     private Transform m_Target;
+	[SerializeField]
+	private Vector3 m_CamOffset;
+    [SerializeField]
     private float m_CurRotationSpeed = 0;
     [SerializeField]
-    private Vector3 m_CamOffset;
+    private bool m_LookAtPlayer = false;
     private bool m_InitFocus = false;
 
     public void Init(Transform target)
@@ -34,15 +37,18 @@ public class CameraController : MonoBehaviour
     public void RotateRandomly()
     {
         int dir = Random.Range(0, 100) > 50 ? -1 : 1;
-        m_CurRotationSpeed = Random.Range(0.5f, 2f) * dir;
+        m_CurRotationSpeed = Time.deltaTime * 45f;//Random.Range(0.5f, 2f) * dir;
+        m_LookAtPlayer = true;
 
-        StartCoroutine(RotatePeriod(Random.Range(0.5f, 2f)));
+        StartCoroutine(RotatePeriod(1));//Random.Range(0.5f, 2f)));
     }
 
     IEnumerator RotatePeriod(float time)
     {
         yield return new WaitForSeconds(time);
         m_CurRotationSpeed = 0;
+        yield return new WaitForSeconds(time);
+        m_LookAtPlayer = false;
     }
 
     void LateUpdate()
@@ -50,13 +56,19 @@ public class CameraController : MonoBehaviour
         if (!GameManager.Instance.IsActive && m_Target == null)
             return;
 
+        //Lerping camera offset
         if (!m_InitFocus && (m_CamOffset - CameraOffset).sqrMagnitude >= 0.1f)
             m_CamOffset = Vector3.Slerp(m_CamOffset, CameraOffset, SmoothFactor);
         else
-            m_InitFocus = true;
+        {
+            if (!m_InitFocus)
+                m_LookAtPlayer = false;
 
-        //Angle and offset
-        Quaternion camTurnAngle = Quaternion.AngleAxis(m_CurRotationSpeed, Vector3.up);
+            m_InitFocus = true;
+        }
+
+		//Angle and offset
+		Quaternion camTurnAngle = Quaternion.AngleAxis(m_CurRotationSpeed, Vector3.up);
         m_CamOffset = camTurnAngle * m_CamOffset;
 
         //Slerp to offset
@@ -64,8 +76,11 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
 
         //Slerp to rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, 
-            Quaternion.LookRotation(m_Target.transform.position - transform.position), Time.deltaTime);
-        //transform.rotation = Quaternion.LookRotation(Player.transform.position - transform.position);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, 
+        //                                      Quaternion.LookRotation(m_Target.transform.position - transform.position), 
+        //                                      Time.deltaTime);
+
+        if (m_LookAtPlayer)
+            transform.LookAt(m_Target);
     }
 }
