3b55a97a167d5ef8bd23bf6eb87d647909a39702
P <elvess65@gmail.com>
2018-05-12 14:53:13 +0300

    1

--- Assets/Scripts/Action/ActionComponents/ActionComponent_Obstacle.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-/// <summary>
-/// Компонент триггера - Преграда
-/// </summary>
-public class ActionComponent_Obstacle : MonoBehaviour
-{
-    public Transform HitPoint;
-}
--- Assets/Scripts/Input/VirtualJoystickInputManager.cs
+++ Assets/Scripts/Input/VirtualJoystickInputManager.cs
@@ -4,15 +4,12 @@ public class VirtualJoystickInputManager : BaseInputManager
 {
     public string MainJoystickName = "MainJoystick";
 
-    private Vector2 m_PrevDir = Vector2.zero;
-
 	public override void UpdateInput() 
     {
         if (!Enabled)
             return;
 
 		Vector2 jPosition = UltimateJoystick.GetPosition(MainJoystickName);
-        m_PrevDir = jPosition;
 
         if (OnMove != null)
             OnMove(new Vector3(jPosition.x, 0, jPosition.y));
