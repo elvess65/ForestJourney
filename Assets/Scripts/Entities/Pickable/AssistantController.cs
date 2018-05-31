58eeb25e3459c81d3dc762b41c67905f0ac53b19
Pavlo <elvess65@gmail.com>
2018-05-31 15:40:19 +0300

    1

--- Assets/Scripts/Entities/Pickable/AssistantController.cs
+++ Assets/Scripts/Entities/Pickable/AssistantController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class AssistantController : Item_Base
@@ -6,15 +7,12 @@ public class AssistantController : Item_Base
 	[Header(" - DERRIVED -")]
     [Header("Settings")]
     public Vector3 AnchorOffset;
-    [Header(" - Idle")]
-    public float IdleAmplitude = 0.2f;
-    public float IdleFrequency = 1;
-    [Header(" - Picked")]
-    [Header("   -  Fosusing")]
+    [Header(" - Interaction")]
+    [Header("   -  Focusing")]
     public float FocusingSpeed = 0.1f;
-    [Header("   -  Vertical Wave")]
-    public float PickedHeightOffset = 1f;
-    public float PickedHeightAmplitude = 2f;
+    [Header(" - Following")]
+    public float RotationSpeed = 2;
+    public float FollowingSpeed = 0.05f;
     [Header(" - Assist")]
     public float TrailTime = 10;
     [Header(" - Assisted")]
@@ -22,23 +20,22 @@ public class AssistantController : Item_Base
     public float SqrDistToDisable = 10;
     [Header("References")]
     public TrailRenderer Trail;
+    public Animator AnimationController;
     public Effect_Particles_StopLoop Effect;
 
-    //Idle
-    private bool m_Idle = true;
-    //Picked
-    private bool m_InitFocus = false;
-    private Vector3 m_CurAnchorOffset;
-    //Way
+    private Behaviour m_Behaviour;
     private NavMeshAgent m_Agent;
-    //Assisted
-    private float m_CurWaitTime = 0;
-    private bool m_WaitingForThePlayerToDisapear = false;
+
+    public NavMeshAgent Agent
+    {
+        get { return m_Agent; }
+    }
 
     protected override void Start() 
     {
+		Trail.enabled = false;
         m_Agent = GetComponent<NavMeshAgent>();
-        Trail.enabled = false;
+        m_Behaviour = new IdleBehaviour(this);
     }
 
     protected override void Update()
@@ -46,76 +43,16 @@ public class AssistantController : Item_Base
         if (!GameManager.Instance.IsActive)
             return;
 
-        if (m_Idle)
-        {
-            transform.position = new Vector3(transform.position.x,
-                                             transform.position.y + Mathf.Sin(Time.time * IdleFrequency) * IdleAmplitude,
-                                             transform.position.z);
-        }
-        else if (m_Picked)
-        {
-            if (!m_InitFocus)
-            {
-                //Move to offset position
-                Vector3 destPos = GameManager.Instance.GameState.Player.transform.position + AnchorOffset;
-                transform.position = Vector3.Slerp(transform.position, destPos, FocusingSpeed);
-
-                if ((destPos - transform.position).sqrMagnitude <= 0.1f)
-                    m_InitFocus = true;
-            }
-            else
-            {
-                //Rotation around
-                Quaternion curAngle = Quaternion.AngleAxis(2, Vector3.up);
-                m_CurAnchorOffset = curAngle * m_CurAnchorOffset;
-
-                //Randomize Y position
-                m_CurAnchorOffset.y = Mathf.Sin(Time.time) * PickedHeightAmplitude + PickedHeightOffset;
-                //Align speed to sin wave (up by amplitude to prevent <0 speed)
-                float speed = Mathf.Sin(Time.time) + 1.1f;
-
-                //Slerp to offset
-                Vector3 newPos = GameManager.Instance.GameState.Player.transform.position + m_CurAnchorOffset;
-                transform.position = Vector3.Slerp(transform.position, newPos, speed);
-            }
-        }
-        else if (m_Using)
-        {
-            if (m_Agent.remainingDistance != Mathf.Infinity && m_Agent.remainingDistance <= 1f && m_Agent.remainingDistance > 0)
-            {
-                m_Using = false;
-                m_WaitingForThePlayerToDisapear = true;
-            }
-        }
-        else if (m_WaitingForThePlayerToDisapear)
-        {
-            float sqrDist = (GameManager.Instance.GameState.Player.transform.position - transform.position).sqrMagnitude;
-
-            //If player not arrived wait time
-            if (sqrDist > SqrDistToDisable)
-            {
-                m_CurWaitTime += Time.deltaTime;
-
-                //If wait time elapsed set dist to 0 (player arrived)
-                if (m_CurWaitTime >= MaxAssistedWaitTime)
-                    sqrDist = 0;
-            }
-
-            if (sqrDist <= SqrDistToDisable)
-            {
-                m_WaitingForThePlayerToDisapear = false;
-                Disable();
-            }
-        }
+        if (m_Behaviour != null)
+            m_Behaviour.Update(Time.deltaTime);
     }
 
     public override void Interact()
     {
         if (GameManager.Instance.GameState.Player.TryAddAssistant(this))
         {
-            m_CurAnchorOffset = AnchorOffset;
             Trail.enabled = true;
-            m_Idle = false;
+            m_Behaviour = new FollowingBehaviour(this);
 
             base.Interact();
         }
@@ -126,13 +63,26 @@ public class AssistantController : Item_Base
         base.Use();
 
         MoveTo(GameManager.Instance.AssistManager.FindNext());
-        m_Using = true;
+        m_Behaviour = new UsingBehaviour(this);
+        m_Behaviour.OnBehaviourFinished += UsingFinishedHandler;
     }
 
-    void MoveTo(Vector3 pos)
+    void UsingFinishedHandler()
     {
-        Trail.time = TrailTime;
-        m_Agent.SetDestination(pos);
+        m_Behaviour = new ArrivedToDestinationBehaviour(this);
+        m_Behaviour.OnBehaviourFinished += ArrivedToDestinationFinishedHandler;
+    }
+
+	void MoveTo(Vector3 pos)
+	{
+		Trail.time = TrailTime;
+		m_Agent.SetDestination(pos);
+	}
+
+
+	void ArrivedToDestinationFinishedHandler()
+    {
+		Disable();
     }
 
     void Disable()
@@ -141,4 +91,115 @@ public class AssistantController : Item_Base
        
         Destroy(gameObject, 5);
     }
+
+	abstract class Behaviour
+	{
+		public System.Action OnBehaviourFinished;
+		protected AssistantController m_Controller;
+
+		public Behaviour(AssistantController controller)
+		{
+			m_Controller = controller;
+		}
+
+		public abstract void Update(float deltaTime);
+	}
+
+	class IdleBehaviour : Behaviour
+	{
+		public IdleBehaviour(AssistantController controller) : base(controller)
+		{
+		}
+
+		public override void Update(float deltaTime)
+		{
+		}
+	}
+
+	class FollowingBehaviour : Behaviour
+	{
+		private bool m_InitFocus = false;
+		private Vector3 m_CurAnchorOffset;
+
+		public FollowingBehaviour(AssistantController controller) : base(controller)
+		{
+			m_CurAnchorOffset = m_Controller.AnchorOffset;
+		}
+
+		public override void Update(float deltaTime)
+		{
+			if (!m_InitFocus)
+			{
+				//Move to offset position
+				Vector3 destPos = GameManager.Instance.GameState.Player.transform.position + m_CurAnchorOffset;
+				m_Controller.transform.position = Vector3.Slerp(m_Controller.transform.position, destPos, m_Controller.FocusingSpeed);
+
+				if ((destPos - m_Controller.transform.position).sqrMagnitude <= 0.1f)
+					m_InitFocus = true;
+			}
+			else
+			{
+				//Rotation around
+				Quaternion curAngle = Quaternion.AngleAxis(m_Controller.RotationSpeed, Vector3.up);
+				m_CurAnchorOffset = curAngle * m_CurAnchorOffset;
+
+				//Align speed to sin wave (up by amplitude to prevent <0 speed)
+				//float speed = Mathf.Sin(Time.time) + 1f;
+
+				//Slerp to offset
+				Vector3 newPos = GameManager.Instance.GameState.Player.transform.position + m_CurAnchorOffset;
+				m_Controller.transform.position = Vector3.Slerp(m_Controller.transform.position, newPos, m_Controller.FollowingSpeed);
+			}
+		}
+	}
+
+	class UsingBehaviour : Behaviour
+	{
+		public UsingBehaviour(AssistantController controller) : base(controller)
+		{
+		}
+
+		public override void Update(float deltaTime)
+		{
+			if (m_Controller.Agent.remainingDistance != Mathf.Infinity && m_Controller.Agent.remainingDistance <= 1f && m_Controller.Agent.remainingDistance > 0)
+			{
+				if (OnBehaviourFinished != null)
+					OnBehaviourFinished();
+				//m_WaitingForThePlayerToDisapear = true;
+			}
+		}
+	}
+
+	class ArrivedToDestinationBehaviour : Behaviour
+	{
+		private float m_CurWaitTime = 0;
+
+		public ArrivedToDestinationBehaviour(AssistantController controller) : base(controller)
+		{
+		}
+
+		public override void Update(float deltaTime)
+		{
+			float sqrDist = (GameManager.Instance.GameState.Player.transform.position - m_Controller.transform.position).sqrMagnitude;
+
+			//If player not arrived wait time
+			if (sqrDist > m_Controller.SqrDistToDisable)
+			{
+				m_CurWaitTime += Time.deltaTime;
+
+				//If wait time elapsed set dist to 0 (player arrived)
+				if (m_CurWaitTime >= m_Controller.MaxAssistedWaitTime)
+					sqrDist = 0;
+			}
+
+			if (sqrDist <= m_Controller.SqrDistToDisable)
+			{
+				if (OnBehaviourFinished != null)
+					OnBehaviourFinished();
+
+
+			}
+		}
+	}
+
 }
