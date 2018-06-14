4d8cc06583419ecdd479626ca796290d67f41acd
Pavlo <elvess65@gmail.com>
2018-06-14 18:43:36 +0300

    1

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,23 +3,36 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Buttons")]
     public Button Button_Assist;
     public Button Button_Weapon;
-
-    private JoystickAnimationController m_JoystickAnimationController;
+    [Header("Animation Controllers")]
+    public UIAnimationController JoystickAnimationController;
+    public UIAnimationController AssistantButtonAnimationController;
+    public UIAnimationController WeaponButtonAnimationController;
+    [Header("Animation Controllers Init settings")]
+    public bool ShowAssistantButton = true;
+    public bool ShowWeaponButton = true;
 
 	void Start ()
     {
-        m_JoystickAnimationController = GetComponent<JoystickAnimationController>();
-        if (m_JoystickAnimationController != null)
-            InputManager.Instance.OnInputStateChange += m_JoystickAnimationController.PlayAnimation;
-
+        //Button
         if (Button_Assist != null)
             Button_Assist.onClick.AddListener(Assist_PressHandler);
 
         if (Button_Weapon != null)
             Button_Weapon.onClick.AddListener(Weapon_PressHandler);
-    }
+
+        //Animation Controller
+		InputManager.Instance.OnInputStateChange += JoystickAnimationController.PlayAnimation;
+
+        if (ShowAssistantButton && AssistantButtonAnimationController != null)
+            InputManager.Instance.OnInputStateChange += AssistantButtonAnimationController.PlayAnimation;
+
+        if (ShowWeaponButton && WeaponButtonAnimationController != null)
+			InputManager.Instance.OnInputStateChange += WeaponButtonAnimationController.PlayAnimation;
+	}
+
 
     public void Assist_PressHandler()
     {
