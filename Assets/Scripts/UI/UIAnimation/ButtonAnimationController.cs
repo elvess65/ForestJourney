6e84b0a84edb24cc2bd117ab0d2b0c2df96a61d5
Pavlo <elvess65@gmail.com>
2018-06-14 18:31:37 +0300

    1

---
+++ Assets/Scripts/UI/UIAnimation/ButtonAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimationController : UIAnimationController 
+{
+    public Button UIButton;
+
+    private RectTransform m_RectTransform;
+
+	protected override void SetTargetPosition()
+	{
+        //Получить текущую позицию кнопки
+        m_RectTransform = UIButton.transform as RectTransform;
+        m_TargetPosition = m_RectTransform.anchoredPosition.x;
+	}
+
+    protected override void ApplyPosition(float position)
+    {
+        m_RectTransform.anchoredPosition = new Vector2(position, m_RectTransform.anchoredPosition.y);
+    }
+}
---
+++ Assets/Scripts/UI/UIAnimation/JoystickAnimationController.cs
@@ -0,0 +1,16 @@
+﻿public class JoystickAnimationController : UIAnimationController 
+{
+    public UltimateJoystick Joystick;
+
+    protected override void SetTargetPosition()
+    {
+		//Получить текущую позицию джойстика
+		m_TargetPosition = Joystick.customSpacing_X;
+    }
+
+	protected override void ApplyPosition(float position)
+	{
+		Joystick.customSpacing_X = position;
+		Joystick.UpdatePositioning();
+	}
+}
