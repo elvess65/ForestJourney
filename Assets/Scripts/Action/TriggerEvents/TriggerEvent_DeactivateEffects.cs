d708962acca3c96cf20e485dd414ca51fffad9cf
Pavlo <elvess65@gmail.com>
2018-06-23 00:46:39 +0300

    1

--- Assets/Scripts/Action/TriggerEvents/TriggerEvent_DeactivateEffects.cs
+++ Assets/Scripts/Action/TriggerEvents/TriggerEvent_DeactivateEffects.cs
@@ -1,13 +1,31 @@
-﻿/// <summary>
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
 /// Отключение эффектов
 /// </summary>
 public class TriggerEvent_DeactivateEffects : TriggerAction_Event
 {
     public Effect_Base[] Effects;
+    public float Delay = 1;
 
     public override void StartEvent()
     {
-        for (int i = 0; i < Effects.Length; i++)
-            Effects[i].Deactivate();
+        if (Delay > 0)
+            StartCoroutine(WaitDelay());
+        else
+            DeactivateEffects();
+    }
+
+    IEnumerator WaitDelay()
+    {
+        yield return new WaitForSeconds(Delay);
+        DeactivateEffects();
+    }
+
+    void DeactivateEffects()
+    {
+		for (int i = 0; i < Effects.Length; i++)
+			Effects[i].Deactivate();
     }
 }
