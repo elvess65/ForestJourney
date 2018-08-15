65a823a28a8f9e338e0f0e48c210e887cc3a2d49
Pavlo <elvess65@gmail.com>
2018-08-15 18:30:47 +0300

    1

---
+++ Assets/Scripts/Effects/ScriptableEffects/DissolveEffectBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class DissolveEffectBehaviour : MonoBehaviour 
+{
+    public float DissolveTime = 3;
+    public float DissolveValue = 0.5f;
+    public MeshRenderer[] MeshRenderers;
+
+    private Material m_Material;
+    private Utils.InterpolationData<float> m_LerpData;
+
+	void Start () 
+    {
+        m_Material = new Material(MeshRenderers[0].sharedMaterial);
+        for (int i = 0; i < MeshRenderers.Length; i++)
+            MeshRenderers[i].sharedMaterial = m_Material;
+
+        m_LerpData = new Utils.InterpolationData<float>();
+        m_LerpData.From = 0;
+        m_LerpData.To = DissolveValue;
+        m_LerpData.TotalTime = DissolveTime;
+	}
+	
+    public void Dissolve()
+    {
+        m_LerpData.Start();
+    }
+
+	void Update () 
+    {
+        if (m_LerpData.IsStarted)
+        {
+            m_LerpData.Increment(Time.deltaTime);
+            float dissolve = Mathf.Lerp(m_LerpData.From, m_LerpData.To, m_LerpData.Progress);
+            m_Material.SetFloat("_SliceAmount", dissolve);
+
+            if (m_LerpData.Overtime())
+                m_LerpData.Stop();
+        }
+	}
+}
+ 
