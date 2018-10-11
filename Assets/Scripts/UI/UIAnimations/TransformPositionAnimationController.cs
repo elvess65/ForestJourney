a07bf0988b733c2daa0039bd92db24d4a6275f49
elvess65 <37631917+elvess65@users.noreply.github.com>
2018-10-11 23:48:53 +0300

    1

---
+++ Assets/Scripts/UI/UIAnimations/TransformPositionAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace mytest.UI.Animations
+{
+    /// <summary>
+    /// Контролирует анимацию расположения Transform
+    /// </summary>
+    public class TransformPositionAnimationController : VectorAnimationController
+    {
+        [Header("Link")]
+        public Transform Target;
+
+        protected override void SetTargetPosition()
+        {
+            //Получить текущую позицию кнопки
+            m_TargetPosition = Target.position;
+        }
+
+        protected override void ApplyPosition(Vector3 position)
+        {
+            //Задать позицию кнопки
+            Target.position = position;
+        }
+    }
+}
